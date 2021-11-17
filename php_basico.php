2c043cb7ac29382ed24a9b5bcf7efdd2f3a5f603
Ste2812 <stefanogallone@hotmail.com>
2021-11-17 11:57:18 +0100

    ejercicio 2 niv 1

--- php_basico.php
+++ php_basico.php
@@ -1,7 +1,9 @@
  <?php
+
+    //ejercicio 1niv1
     $valorX=7;
     $cadena="Hello world!";
-    $verdadero= false;
+    $verdadero= true;
     $valorY=25.35;
     
     echo "numero entero del valor X=" .$valorX;
@@ -11,5 +13,24 @@
     echo $cadena;
     echo "<br>";
     print "el valor booleano es: $verdadero";
-    echo $verdadero;
+    echo "<br>";
+    echo "<hr>";
+
+    #ejercicio 2niv1
+
+    $cadena_texto= "Hello world!";
+
+    echo $cadena_texto;
+    echo "<br>";
+    echo strtoupper($cadena_texto);
+    echo "<br>";
+    echo strlen($cadena_texto);
+    echo "<br>";
+    echo strrev($cadena_texto);
+    echo "<br>";
+    
+    $cadena_texto2= "Esto es el curso de PHP";
+    echo $cadena_texto." ".$cadena_texto2;
+    echo "<br>";
+    echo "<hr>";    
 ?>
