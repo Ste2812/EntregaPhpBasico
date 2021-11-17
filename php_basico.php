aa3966d9cc9caa81f0f9a7d2efc092f5cd331b81
Ste2812 <stefanogallone@hotmail.com>
2021-11-17 10:46:38 +0100

    ejercicio 1 niv 1

---
+++ php_basico.php
@@ -0,0 +1,15 @@
+ <?php
+    $valorX=7;
+    $cadena="Hello world!";
+    $verdadero= false;
+    $valorY=25.35;
+    
+    echo "numero entero del valor X=" .$valorX;
+    echo "<br>";
+    echo "numero decimal del valor Y= $valorY";
+    echo "<br>";
+    echo $cadena;
+    echo "<br>";
+    print "el valor booleano es: $verdadero";
+    echo $verdadero;
+?>
